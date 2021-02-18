6bc8e240e85dfc1981395b695e5858226f3dcded
Termitsforge <61342523+Termitsforge@users.noreply.github.com>
2021-02-18 12:36:06 +0300

    first commit

---
+++ Lab1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab1
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет пидорас!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab1/class2.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Lab1
+{
+    public class class2
+    {
+        
+    }
+}
